6d6f0f2b0e4bfdf5851d1c552dacde30b5ab3ce5
ilikecoffee18 <cherish.kim@students.makeschool.com>
2018-12-21 03:16:09 -0500

    added MongoDB and Client model

--- app.js
+++ app.js
@@ -1,18 +1,33 @@
 const express = require('express');
 const app = express();
 var exphbs = require('express-handlebars');
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/charity-directory')
 
-//mock array
-let clients = [
-  { name: "John Smith", description: "likes animals" },
-  { name: "Ella Hill", description: "environmentalist" }
-]
+//Makin a model! The M in MVC. The Data Layer. This model lets us write and read data from MongoDB database with Mongoose
+const Client = mongoose.model('Client', {
+  name: String,
+  description: String
+});
+
+// //mock array
+// let clients = [
+//   { name: "John Smith", description: "likes animals" },
+//   { name: "Ella Hill", description: "environmentalist" }
+// ]
 
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
 
+// INDEX
 app.get('/', (req, res) => {
-  res.render('clients-index', { clients: clients });
+  Client.find()
+    .then(clients => {
+      res.render('clients-index', { clients: clients });
+    })
+    .catch(err => {
+      console.log(err);
+    })
 });
 
 app.listen(3000, () => {
