61d75a119fefa18ace04d1c621e41e66136854d4
ilikecoffee18 <cherish.kim@students.makeschool.com>
2018-12-21 02:06:47 -0500

    users can see all clients

--- app.js
+++ app.js
@@ -2,11 +2,17 @@ const express = require('express');
 const app = express();
 var exphbs = require('express-handlebars');
 
+//mock array
+let clients = [
+  { name: "John Smith", description: "likes animals" },
+  { name: "Ella Hill", description: "environmentalist" }
+]
+
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
 
 app.get('/', (req, res) => {
-  res.render('home', { msg: 'Handlebars are Cool!' });
+  res.render('clients-index', { clients: clients });
 });
 
 app.listen(3000, () => {
